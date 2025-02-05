6a4f0e92f1783b77795799ece0209b87b07d661b
Daniela Castro <115217686+arbyun@users.noreply.github.com>
2025-02-05 13:40:13 +0000

    add bow

---
+++ Assets/Scripts/Tools/Bow.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+namespace Tools
+{
+    public class Bow : XRBaseInteractable
+    {
+        public static event Action<float> PullActionReleased;
+        
+        [SerializeField] private Transform start;
+        [SerializeField] private Transform end;
+        [SerializeField] private LineRenderer bowString;
+
+        private float _pullVal;
+        private IXRSelectInteractor _interactor;
+
+        public void SetPull(SelectEnterEventArgs args)
+        {
+            _interactor = args.interactorObject;
+        }
+
+        public void Release()
+        {
+            PullActionReleased?.Invoke(_pullVal);
+            _interactor = null;
+            _pullVal = 0;
+            UpdateString();
+        }
+
+        public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+        {
+            base.ProcessInteractable(updatePhase);
+
+            if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
+            {
+                if (isSelected)
+                {
+                    Vector3 pullPos = _interactor.transform.position;
+                    _pullVal = CalculatePull(pullPos);
+                    UpdateString();
+                }
+            }
+        }
+
+        private void UpdateString()
+        {
+            Vector3 linePos = Vector3.forward *
+                              Mathf.Lerp(start.transform.localPosition.z, end.transform.localPosition.z, _pullVal);
+            bowString.SetPosition(1, linePos);
+        }
+
+        private float CalculatePull(Vector3 pullPos)
+        {
+            Vector3 pullDir = pullPos - start.position;
+            Vector3 targetDir = end.position - start.position;
+            float maxLength = targetDir.magnitude;
+            
+            targetDir.Normalize();
+            float pullValue = Vector3.Dot(pullDir, targetDir) / maxLength;
+            return Mathf.Clamp(pullValue, 0, 1);
+        }
+    }
+}
